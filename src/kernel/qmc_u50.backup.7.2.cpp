00e1aa67e024e8040f6b998f1c3111b37cbdba6e
allen880117 <wangallen880117@gmail.com>
2021-11-28 15:54:54 +0800

    Rename some of kernels and using NUM_FADD to specify the number of fadd

--- src/kernel/qmc_u50.backup.7.2.cpp
+++ src/kernel/qmc_u50.backup.7.2.cpp
@@ -132,7 +132,7 @@ SUM_UP:
     for (u32_t ofst = 0, pack_ofst = 0; ofst < NUM_SPIN / PACKET_SIZE / NUM_STREAM;
          ofst++, pack_ofst += NUM_STREAM) {
         // Pramgas: Pipeline and Confine the usage of fadd
-        CTX_PRAGMA(HLS ALLOCATION operation instances = fadd limit = 64)
+        CTX_PRAGMA(HLS ALLOCATION operation instances = fadd limit = NUM_FADD)
         CTX_PRAGMA(HLS PIPELINE)
 
         // Buffer for source of adder
