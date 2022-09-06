ab8a832dfa6692e4886d75be0ea289296739a1c1
allen880117 <wangallen880117@gmail.com>
2022-09-06 18:13:09 +0800

    src: refactor csim_main, using fixed-random-seed at csim_helper

--- src/sqa.hpp
+++ src/sqa.hpp
@@ -43,15 +43,13 @@ typedef ap_uint<PACKET_SIZE> qubitPack_t;
 
 /* Prototype of Functions */
 extern "C" {
-void RunSQAHardware(u32_t nTrotters, u32_t nQubits, u32_t nSteps, fp_t jperp,
-                    fp_t hCache[MAX_QUBIT_NUM], fp_t rndNumMem[MAX_TROTTER_NUM][NUM_COL_RNDNUM_MEM],
+void RunSQAHardware(u32_t nTrotters, u32_t nQubits, fp_t jperp, fp_t hCache[MAX_QUBIT_NUM],
+                    fp_t        rndNumMem[MAX_TROTTER_NUM][NUM_COL_RNDNUM_MEM],
                     fpPack_t    jcoupMemBank0[MAX_QUBIT_NUM][NUM_COL_JCOUP_MEM_BANK],
                     fpPack_t    jcoupMemBank1[MAX_QUBIT_NUM][NUM_COL_JCOUP_MEM_BANK],
                     qubitPack_t qubitsCache[MAX_TROTTER_NUM][NUM_COL_QUBIT_CACHE]);
 }
 
-void RunSQASoftware(const int nTrot, const int nSpin,
-                    qubit_t    trotters[MAX_TROTTER_NUM][MAX_QUBIT_NUM],
-                    const fp_t Jcoup[MAX_QUBIT_NUM][MAX_QUBIT_NUM], const fp_t h[MAX_QUBIT_NUM],
-                    const fp_t jperp, const fp_t Beta,
-                    const fp_t logRandNumber[MAX_TROTTER_NUM][MAX_QUBIT_NUM]);
+void RunSQASoftware(u32_t nTrotters, u32_t nQubits, qubit_t qubits[MAX_TROTTER_NUM][MAX_QUBIT_NUM],
+                    fp_t jcoup[MAX_QUBIT_NUM][MAX_QUBIT_NUM], fp_t h[MAX_QUBIT_NUM], fp_t jperp,
+                    fp_t beta, fp_t logRndNum[MAX_TROTTER_NUM][MAX_QUBIT_NUM]);
--- src/sqa_sw.cpp
+++ src/sqa_sw.cpp
@@ -1,31 +1,28 @@
 #include "sqa.hpp"
 
 /* Quantum Monte-Carlo */
-void RunSQASoftware(const int nTrot, const int nSpin,
-                    qubit_t    trotters[MAX_TROTTER_NUM][MAX_QUBIT_NUM],
-                    const fp_t Jcoup[MAX_QUBIT_NUM][MAX_QUBIT_NUM],
-                    const fp_t h[MAX_QUBIT_NUM], const fp_t jperp,
-                    const fp_t Beta,
-                    const fp_t logRandNumber[MAX_TROTTER_NUM][MAX_QUBIT_NUM])
+void RunSQASoftware(u32_t nTrotters, u32_t nQubits, qubit_t qubits[MAX_TROTTER_NUM][MAX_QUBIT_NUM],
+                    fp_t jcoup[MAX_QUBIT_NUM][MAX_QUBIT_NUM], fp_t h[MAX_QUBIT_NUM], fp_t jperp,
+                    fp_t beta, fp_t logRndNum[MAX_TROTTER_NUM][MAX_QUBIT_NUM])
 {
     /* Tunnel-related energy */
     // fp_t dHTunnel = 2.0f * jperp * nTrot;
-    fp_t dHTunnel = 2.0f * 0.5f * jperp * (fp_t)nTrot;
+    fp_t dHTunnel = 2.0f * 0.5f * jperp * (fp_t)nTrotters;
 
 /* Traverse all trotters */
 LOOP_TROTTERS:
     for (int m = 0; m < MAX_TROTTER_NUM; m++) {
         /* Exit condition */
-        if (m == nTrot) break;
+        if (m == nTrotters) break;
 
     /* Traverse all spins in trotter m */
     LOOP_SPINS:
         for (int i = 0; i < MAX_QUBIT_NUM; i++) {
             /* Exit condition */
-            if (i == nSpin) break;
+            if (i == nQubits) break;
 
             /* Get this spin */
-            qubit_t this_spin = trotters[m][i];
+            qubit_t this_spin = qubits[m][i];
 
             /* Local Field of spin(m, i, j) */
             fp_t dH = 0;
@@ -34,18 +31,18 @@ LOOP_TROTTERS:
         LOOP_COUPLES:
             for (int j = 0; j < MAX_QUBIT_NUM; j++) {
                 /* Exit Condition */
-                if (j == nSpin) break;
-                if (trotters[m][j])
-                    dH += Jcoup[i][j];
+                if (j == nQubits) break;
+                if (qubits[m][j])
+                    dH += jcoup[i][j];
                 else
-                    dH -= Jcoup[i][j];
+                    dH -= jcoup[i][j];
             }
 
             /* Compute Engery from up and down trotter */
-            int     up           = (m == 0) ? (nTrot - 1) : (m - 1);
-            int     down         = (m == nTrot - 1) ? (0) : (m + 1);
-            qubit_t up_trotter   = trotters[up][i];
-            qubit_t down_trotter = trotters[down][i];
+            int     up           = (m == 0) ? (nTrotters - 1) : (m - 1);
+            int     down         = (m == nTrotters - 1) ? (0) : (m + 1);
+            qubit_t up_trotter   = qubits[up][i];
+            qubit_t down_trotter = qubits[down][i];
             if (up_trotter == down_trotter) {
                 if (up_trotter)
                     dH -= dHTunnel;
@@ -64,9 +61,7 @@ LOOP_TROTTERS:
             if (!this_spin) { dH = -dH; }
 
             /* Flip */
-            if ((-Beta * dH) > logRandNumber[m][i]) {
-                trotters[m][i] = (!trotters[m][i]);
-            }
+            if ((-beta * dH) > logRndNum[m][i]) { qubits[m][i] = (!qubits[m][i]); }
         }
     }
 }
